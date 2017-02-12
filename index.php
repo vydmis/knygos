0647b5559687e0e9ba9ed0ea7934e0f302f012e7
vydmis <vydmis@gmail.com>
2017-02-12 21:27:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,63 @@
-
+<?php
+setlocale(LC_ALL, "lithuanian");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+        <title>Knygos</title>
+        <!--<meta charset="utf-8" />-->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="inc/style.css" />
+    </head>
+    <body> 
+        <div class="row header">
+            <div class="container">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <!--   Virsutine dalis -->
+                        <h1>Knygos</h1>
+                        <span class="slogan">Knygų sąrašas</span>
+                        <!-- Virsutine dalis -->
+                    </div>
+                </div>
+            </div>     
+        </div>
+        <div class="container">
+            <div class="row">
+                <div id="nav" class="col-md-12">
+                    <!-- Navigacija -->
+                    <?php
+                    
+                    ?>
+                    <!-- Navigacija -->
+                </div>
+            </div>
+        </div>   
+        <div class="container">
+            <div class="row">
+                
+                <div id="content" class="col-md-12">
+                    <!--  Contentas  -->
+                    <?php
+                        
+                    ?>	
+                    <!-- Contentas -->
+                </div>
+            </div>
+        </div>    
+        <!--  Footeris-->
+        <div class="container">
+            <div id="footer">
+                <div class="row">
+                </div>
+            </div>
+        </div>    
+        <!--  Footeris-->
+    </body>
+</html>
